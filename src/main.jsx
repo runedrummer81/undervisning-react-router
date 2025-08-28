e6dcb2af141f836307abdde791a33ce4533e6e89
runedrummer81 <runedrummer81@gmail.com>
2025-08-28 15:03:27 +0200

    does it work

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
+// main.jsx or App.jsximport React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter
+      basename={import.meta.env.DEV ? "/" : "/undervisning-react-router/"}
+    >
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
