4dc8744a99f290f5d75f5f7e9e14d5dd547b8136
runedrummer81 <runedrummer81@gmail.com>
2025-08-28 15:07:36 +0200

    testing 2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,10 @@ export default function HomePage() {
   return (
     <section className="page">
       <h1>Velkommen til forsiden!</h1>
-      <p>Dette er en moderne React SPA bygget med Vite og React Router.</p>
+      <p>
+        Dette er en moderne React SPA bygget med Vite og React Router. Det lærte
+        vi om i dagens lektion
+      </p>
     </section>
   );
 }
